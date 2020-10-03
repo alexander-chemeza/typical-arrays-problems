98daf5bd40bbcc79c150598a27fd6317c3ca512d
alexander-chemeza <alexander.chemeza@gmail.com>
2020-10-03 15:43:21 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
+// exports.min = function min (array) {
+//   return 0;
+// }
+
+// exports.max = function max (array) {
+//   return 0;
+// }
+
+// exports.avg = function avg (array) {
+//   return 0;
+// }
+
 exports.min = function min (array) {
-  return 0;
+  let result = Math.min(...array);
+  return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let result = Math.max(...array);
+  return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let total = 0;
+  for(let i =0; i < array.length; i++) {
+    total += array[i];
+  }
+
+  let result = total / array.length;
+  return result;
 }
